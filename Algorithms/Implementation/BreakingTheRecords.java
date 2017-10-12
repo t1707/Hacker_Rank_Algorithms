3b3bb0966db97d27d8a116ec670e7c042d80ace8
t1707 <tapanvaishnav17@gmail.com>
2017-10-12 16:06:37 +0900

    Create BreakingTheRecords.java

---
+++ Algorithms/Implementation/BreakingTheRecords.java
@@ -0,0 +1,77 @@
+/*
+Maria plays  games of college basketball in a season. Because she wants to go pro, she tracks her points scored per game sequentially in an array defined as . After each game , she checks to see if score  breaks her record for most or least points scored so far during that season.
+
+Given Maria's array of  for a season of  games, find and print the number of times she breaks her record for most and least points scored during the season.
+
+Note: Assume her records for most and least points at the start of the season are the number of points scored during the first game of the season.
+
+Input Format
+
+The first line contains an integer denoting  (the number of games). 
+The second line contains  space-separated integers describing the respective values of .
+
+Constraints
+
+Output Format
+
+Print two space-seperated integers describing the respective numbers of times her best (highest) score increased and her worst (lowest) score decreased.
+
+Sample Input 0
+
+9
+10 5 20 20 4 5 2 25 1
+Sample Output 0
+
+2 4
+Explanation 0
+
+The diagram below depicts the number of times Maria broke her best and worst records throughout the season:
+
+image
+
+She broke her best record twice (after games  and ) and her worst record four times (after games , , , and ), so we print 2 4 as our answer. Note that she did not break her record for best score during game , as her score during that game was not strictly greater than her best record at the time.
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] getRecord(int[] s){
+        int min=s[0];
+        int max=s[0];
+        int[] count={0,0};
+        for(int i=1;i<s.length;i++)
+        {
+            if(s[i]<min){
+                min=s[i];
+                count[1]++;
+            }
+            if(s[i]>max)
+            {
+                max=s[i];
+                count[0]++;
+            }
+        }
+        
+        return count;
+    }  
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int[] result = getRecord(s);
+        String separator = "", delimiter = " ";
+        for (Integer val : result) {
+            System.out.print(separator + val);
+            separator = delimiter;
+        }
+        System.out.println("");
+    }
+}
